d30f7a2fa151a05dc4654c38c7646ea1ea8b422c
Retro6790 <114397005+987GHJKLZXCVBNM@users.noreply.github.com>
2023-01-16 17:34:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+/*let minute = 45
+if (15 >= minute && minute > 0){
+    console.log('первая')
+}
+else if (30 >= minute && minute > 15){
+    console.log('вторая')
+}
+else if (45 >= minute && minute > 30){
+    console.log('третья')
+}
+else if (59 >= minute && minute > 45){
+    console.log('четвёртая')
+}*/
+
+/*let num = 5
+let result = (num == 5)?console.log(true):console.log(false);*/
+
+/*let lang = 'en'
+let arr
+if (lang == 'ru'){
+    arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+}
+else if(lang == 'en'){
+    arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']
+}
+for(i = 0;i < 7;i++){
+    console.log(arr[i])
+}*/
+
+/*let input = prompt()
+
+
+if (input >= 1 && input <= 9){
+    console.log(`${input} однозначное положительное`)
+}
+else if (input >= 10 && input <= 99){
+    console.log(`${input} двузначное положительное`)
+}
+else if (input >= 100 && input <= 999){
+    console.log(`${input} трёхзначное положительное`)
+}
+else if (input >= 1000){
+    console.log(`${input} более положительное`)
+}
+
+
+else if (input <= -1 && input >= -9){
+    console.log(`${input} однозначное отрицательное`)
+}
+else if (input <= -10 && input >= -99){
+    console.log(`${input} двузначное отрицательное`)
+}
+else if (input <= -100 && input >= -999){
+    console.log(`${input} трёхзначное отрицательное`)
+}
+else if (input <= -1000){
+    console.log(`${input} более отрицательное`)
+}*/
+
+/*let red
+let yellow
+if (red){
+    console.log('красный')
+}
+else if (yellow){
+    console.log('жёлтый')
+}
+else{
+    console.log('зелёный')
+}*/
+
+/*let result = 1
+while(result <= 12){
+    console.log(result);
+    result++;
+}*/
+
+/*let result = 5
+while(result <= 13){
+    console.log(result);
+    result++;
+}*/
+
+let arr = [2, 5, 8, 3, 6]
+for (i = 0;i <15;i++){
+    
+}
